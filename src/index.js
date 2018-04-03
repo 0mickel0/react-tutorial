fa0ee4e735de4ea7adf1a14b6d1edb9df1f3cdfe
Misha <mihail.nazarov@sustems.de>
2018-04-03 17:13:49 +0300

    add video player

--- src/index.js
+++ src/index.js
@@ -3,24 +3,32 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDefault from './components/video_default'
 
 const API_KEY = 'AIzaSyBA_NXCC1CQNh5ZERViiyy8LD4Jc1jjV4Y';
-
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {videos: ''};
-    YTSearch({key: API_KEY, term: 'serf'}, videos => this.setState({videos}))
-  }
-
-  render(){
-      return  (
-        <div>
-          <SearchBar />
-          <VideoList videos={this.state.videos}/>
-        </div>
-      );
+    constructor(props){
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({key: API_KEY, term: 'serf'}, videos => this.setState({
+            videos:videos,
+            selectedVideo: videos[0]
+        }))
+    }
+    render(){
+        return  (
+            <div>
+                <SearchBar />
+                <VideoDefault video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
     }
 };
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('#root'));
